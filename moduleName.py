1f75374439d4fa9d3ea8b00e6e24ac55e7b7d830
yong_ouyang <yong_ouyang@crfchina.com>
2018-04-13 18:35:22 +0800

    模块---导入自定义模块

---
+++ moduleName.py
@@ -0,0 +1,14 @@
+# 模块
+def funcHello():
+    print("Hello,模块！")
+	
+def fib(n): # return Fibonacci series up to n
+   result = []
+   a, b = 0, 1
+   while b < n:
+      result.append(b)
+      a, b = b, a + b
+   return result
+if __name__ == "__main__":
+   f = fib(100)
+   print(f)
